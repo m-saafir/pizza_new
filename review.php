2e0f8b137c649050f86734caccca9c05979c3f47
Malik Saafir <msaafir99@gmail.com>
2017-05-06 20:03:52 -0400

    get post information from form

---
+++ review.php
@@ -0,0 +1,18 @@
+<?php
+  require('requires.php');
+  $order_type_cd = isset($_REQUEST['order_type']) ? $_REQUEST['order_type'] : null;
+  $size_id = isset($_REQUEST['size']) ? $_REQUEST['size'] : null;
+  $cheese_id = isset($_REQUEST['cheese']) ? $_REQUEST['cheese'] : null;
+  $meat_ids = isset($_REQUEST['meats']) ? $_REQUEST['meats'] : null;
+  $veggie_ids = isset($_REQUEST['veggies']) ? $_REQUEST['veggies'] : null;
+  $fruit_ids = isset($_REQUEST['fruits']) ? $_REQUEST['fruits'] : null;
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+  </body>
+</html>
