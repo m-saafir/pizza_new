74a007059662ee91d9a6e55bf789f45d1b33e691
Malik Saafir <msaafir99@gmail.com>
2017-05-10 20:47:24 -0400

    link to custom css

--- review.php
+++ review.php
@@ -30,6 +30,7 @@
     <meta charset="utf-8">
     <title></title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.min.css">
   </head>
   <body>
     <div class="container container-fluid col-md-9">
