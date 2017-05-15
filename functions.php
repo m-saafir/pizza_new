0dd0c15eb5d2f749ec47c15537390c4a9649f23b
Malik Saafir <msaafir99@gmail.com>
2017-05-15 13:31:30 -0400

    save order and order detail

--- functions.php
+++ functions.php
@@ -20,14 +20,25 @@
     return $result;
   }
 
-  function save_pizza($price, array $topping_ids, mysqli $connection) {
+  function save_pizza($price, $size_id, $order_type_cd, array $topping_ids, mysqli $connection) {
     $pizza_sql = "INSERT INTO p_pizza (price) VALUES ($price)";
     gen_sql($pizza_sql, $connection);
     $pizza_id = $connection->insert_id;
+
+    $customer_sql = "INSERT INTO p_customer (first_name, last_name, email_id) VALUES ('BLAH', 'BLAH', 'blah@blah.com')";
+    gen_sql($customer_sql, $connection);
+    $customer_id = $connection->insert_id;
+
+    $orders_sql = "INSERT INTO p_orders (order_date, customer_id, order_type_cd, order_status_cd) VALUES ('2017-05-15', $customer_id, $order_type_cd, 1)";
+    gen_sql($orders_sql, $connection);
+    $order_id = $connection->insert_id;
 
     foreach ($topping_ids as $topping) {
       $topping_sql = "INSERT INTO p_pizza_topping (pizza_id, topping_id) VALUES ($pizza_id, $topping)";
       gen_sql($topping_sql, $connection);
     }
+
+    $order_details_sql = "INSERT INTO p_order_details (order_id, pizza_id, size_id, pizza_topping_id, pizza_price) VALUES ($order_id, $pizza_id, $size_id, $pizza_id, $price)";
+    gen_sql($order_details_sql, $connection);
   }
 ?>
