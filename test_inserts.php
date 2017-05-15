071758d9cf48aff8c5b934f8952843b89770b251
Malik Saafir <msaafir99@gmail.com>
2017-05-15 13:33:21 -0400

    test function

--- test_inserts.php
+++ test_inserts.php
@@ -1,32 +1,9 @@
 <?php
   include('requires.php');
 
-  $description = 'supreme';
-  $size = 2;
-  $toppings = [1,2,3,4,5];
+  $sizeID = 3; //Large
+  $toppingIDs = [1,2,5,7,8,10];
+  $price = 14.50;
 
-  function savePizza($description, $size, $toppings, $connection) {
-    $price = getPriceOfPizza($toppings);
-
-    $pizza_sql = "INSERT INTO p_pizza (pizza_desc, price) VALUES ('hello', $price)";
-    echo $pizza_sql;
-    gen_sql($pizza_sql, $connection);
-    $pizzaID = $connection->insert_id;
-
-    foreach ($toppings as $topping) {
-      savePizzaTopping($pizzaID, $topping, $connection);
-    }
-  }
-
-  function savePizzaTopping($id, $topping, $connection) {
-    $sql = "INSERT INTO p_pizza_topping (pizza_id, topping_id) VALUES ($id, $topping)";
-    $connection->query($sql);
-  }
-
-  function getPriceOfPizza($toppings) {
-    return 4 * sizeof($toppings);
-  }
-
-  savePizza($description, $size, $toppings, $conn);
-  echo $GLOBALS['error'];
+  save_pizza($price, $sizeID, 1, $toppingIDs, $conn);
 ?>
