2c044006cd5f5fb883a9cef23385aea412098040
Malik Saafir <msaafir99@gmail.com>
2017-05-17 11:58:20 -0400

    test cases

--- test_inserts.php
+++ test_inserts.php
@@ -1,9 +1,7 @@
 <?php
   include('requires.php');
 
-  $sizeID = 3; //Large
-  $toppingIDs = [1,2,5,7,8,10];
-  $price = 14.50;
-
-  save_pizza($price, $sizeID, 1, $toppingIDs, $conn);
+  save_pizza(14.50, 2, 2, [1,3,4,5,6,10], $conn);
+  save_pizza(11.50, 1, 1, [2,4,5,6,10], $conn);
+  save_pizza(18.00, 3, 2, [1,2,3,4,10,11], $conn);
 ?>
