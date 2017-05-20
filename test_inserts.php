bc3bc3a3e36a4fe5a999d5cdf73a6f63c25b508f
Malik Saafir <msaafir99@gmail.com>
2017-05-20 10:38:03 -0400

    not needed anymore

--- test_inserts.php
+++
@@ -1,7 +0,0 @@
-<?php
-  include('requires.php');
-
-  save_pizza(14.50, 2, 2, [1,3,4,5,6,10], $conn);
-  save_pizza(11.50, 1, 1, [2,4,5,6,10], $conn);
-  save_pizza(18.00, 3, 2, [1,2,3,4,10,11], $conn);
-?>
