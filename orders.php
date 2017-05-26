9aca67058b41b9a648bf3e8639c4063b2e88d7e8
Malik Saafir <msaafir99@gmail.com>
2017-05-26 08:56:38 -0400

    create orders page

---
+++ orders.php
@@ -0,0 +1,12 @@
+<? require('requires.php'); ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+  </head>
+  <body>
+
+  </body>
+</html>
