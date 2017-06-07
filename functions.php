8a9d4a6ef0f4834f1783b80b3aaf2ef35e7edaa3
Malik Saafir <msaafir99@gmail.com>
2017-06-07 17:40:21 -0400

    error checking

--- functions.php
+++ functions.php
@@ -22,23 +22,33 @@
 
   function save_pizza(float $price, int $size_id, int $order_type_cd, array $topping_ids, mysqli $connection) {
     $pizza_sql = "INSERT INTO p_pizza (price) VALUES ($price)";
-    gen_sql($pizza_sql, $connection);
+    if (!gen_sql($pizza_sql, $connection)) {
+      return 0;
+    }
     $pizza_id = $connection->insert_id;
 
     $customer_sql = "INSERT INTO p_customer (first_name, last_name, email_id) VALUES ('BLAH', 'BLAH', 'blah@blah.com')";
-    gen_sql($customer_sql, $connection);
+    if (!gen_sql($customer_sql, $connection)) {
+      return 0;
+    }
     $customer_id = $connection->insert_id;
 
     $orders_sql = "INSERT INTO p_orders (order_date, order_type_cd, order_status_cd, customer_id) VALUES (CURRENT_DATE, $order_type_cd, 1, $customer_id)";
-    gen_sql($orders_sql, $connection);
+    if (gen_sql($orders_sql, $connection)) {
+      return 0;
+    }
     $order_id = $connection->insert_id;
 
     foreach ($topping_ids as $topping) {
       $topping_sql = "INSERT INTO p_pizza_topping (pizza_id, topping_id) VALUES ($pizza_id, $topping)";
-      gen_sql($topping_sql, $connection);
+      if (gen_sql($topping_sql, $connection)) {
+        return 0;
+      }
     }
 
     $order_details_sql = "INSERT INTO p_order_details (order_id, pizza_id, size_id, pizza_topping_id, pizza_price) VALUES ($order_id, $pizza_id, $size_id, $pizza_id, $price)";
-    gen_sql($order_details_sql, $connection);
+    if (gen_sql($order_details_sql, $connection)) {
+      return 0;
+    }
   }
 ?>
